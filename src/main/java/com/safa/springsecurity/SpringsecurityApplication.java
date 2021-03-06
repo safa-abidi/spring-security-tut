0b132aba0fca4a6031ebf04e4dbe6312d524bcf4
Safa Abidi <61377020+safa-abidi@users.noreply.github.com>
2021-03-06 17:58:20 +0100

    final

---
+++ src/main/java/com/safa/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,15 @@
+package com.safa.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/safa/springsecurity/UserRepository.java
@@ -0,0 +1,10 @@
+package com.safa.springsecurity;
+
+import com.safa.springsecurity.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
